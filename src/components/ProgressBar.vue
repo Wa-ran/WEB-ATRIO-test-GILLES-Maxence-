71350616cea0c150c78041ee20a825832e551b03
Wa-ran <mgs.gilles@gmail.com>
2022-11-07 18:57:33 +0100

    Fin du test

---
+++ src/components/ProgressBar.vue
@@ -0,0 +1,80 @@
+<template>
+  <div>
+    <div class="title">
+      {{ props.title }}
+    </div>
+    <div class="container">
+      <div class="percent">
+        {{ props.progress }}%
+      </div>
+      <div class="bar">
+        <div
+          class="progress"
+          :class="progressColor()"
+          :style="`width: ${props.progress}%`"
+        ></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+
+const props = defineProps({
+  title: { 
+    type: String,
+    default: 'Barre de progression',
+  },
+  progress: { 
+    type: Number,
+    default: 0,
+  },
+});
+
+const progressColor = () => {
+  if (props.progress > 75) return 'p75'
+  else if (props.progress > 50) return 'p50'
+  else if (props.progress > 25) return 'p25'
+  else return 'p00'
+};
+</script>
+
+<style lang="scss" scoped>
+.title {
+  font-size: 14px;
+  margin-bottom: 10px;
+}
+
+.container {
+  min-width: 100%;
+  display: flex;
+  align-items: center;
+}
+.percent {
+  font-size: 20px;
+  width: 50px;
+}
+.bar {
+  background-color: #373737;
+  height: 15px;
+  box-shadow: 0px 3px 6px #00000029;
+  flex-grow: 1;
+}
+.progress {
+  background-color: #fff;
+  height: 100%;
+}
+.p75 {
+  background-color: #30DB63;
+}
+.p50 {
+  background-color: #60E8B6;
+}
+.p25 {
+  background-color: #60ADE8;
+}
+.p00 {
+  background-color: #7160E8;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,99 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="title">
+      <p>
+        Test technique WEB-ATRIO réalisé par GILLES Maxence
+        <br />
+        réalisé le 07/11/2022
+      </p>
+    </div>
+
+    <div
+      v-for="bar, index of barsMap"
+      :key="index"
+      class="bars"
+    >
+      <ProgressBar
+        :title="bar.title"
+        :progress="bar.progress"
+      />
+    </div>
+
+    <div class="buttons">
+      <button @click="setProgress()">
+        <span>Remettre à zéro les compteurs</span>
+      </button>
+      <button @click="setProgress(5)">
+        <span>Ajouter 5%</span>
+      </button>
+      <button @click="setProgress(10)">
+        <span>Ajouter 10%</span>
+      </button>
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<script setup>
+import ProgressBar from '@/components/ProgressBar.vue'
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+import { ref } from "vue";
+
+const barsMap = ref([
+  {
+    title: 'Initialisation du test technique',
+    progress: 50
+  },
+  {
+    title: 'Avancement de la phase de développement',
+    progress: 25
+  },
+]);
+
+const setProgress = (prog) => {
+  for (let bar of barsMap.value) {
+    if (prog) {
+      if ((bar.progress += prog) > 100) bar.progress = 100
+    }
+    else bar.progress = 0
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.container {
+  > * {
+    margin: 80px;
+  }
+}
+
+.title {
+  text-align: center;
+  font-weight: bold;
+  font-size: 16px;
+}
+
+.bars {
+  display: flex;
+  flex-direction: column;
+  max-width: 840px;
+  margin: auto;
+  > div {
+    margin: 20px;
+  }
+}
+
+.buttons {
+  display: flex;
+  justify-content: center;
+}
+button {
+  cursor: pointer;
+  background-color: #373737;
+  border: none;
+  box-shadow: 0px 3px 6px #00000029;
+  font-size: 14px;
+  padding: 10px;
+  margin: 15px;
+  color: currentColor;
+}
+</style>
